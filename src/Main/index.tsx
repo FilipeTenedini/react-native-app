af63041eed44b2e065b42de41cf234af185d23f4
FilipeTenedini <tenedinifilipe@gmail.com>
2023-11-08 01:09:50 -0300

    feat: use main at App and get OS for set margins

---
+++ src/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../components/Header';
+import { Container } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Main;
